66d2fd04d0991002d141390ff4c24e5e891e28ea
Your Name <lewisjassy43@gmail.com>
2023-03-16 04:30:50 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+1  #include<stdio.h>
+2  
+3  /**
+4  * main - Calls puts and prints string
+5  *
+6  * Return : 0 on succes
+7  */
+8  int main(void)
+9  {
+10	puts("\"Programming is like building a multilingual puzzle");
+11	return(0);
+12}
