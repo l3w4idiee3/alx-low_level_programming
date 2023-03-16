eaa10ae281e8d791cdb1464c916502311b36b0e2
Your Name <lewisjassy43@gmail.com>
2023-03-16 04:51:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-1  #include<stdio.h>
-2  
-3  /**
-4  * main - Calls puts and prints string
-5  *
-6  * Return : 0 on succes
-7  */
-8  int main(void)
-9  {
-10	puts("\"Programming is like building a multilingual puzzle");
-11	return(0);
-12}
+#include <stdio.h>
+
+/**
+ * main - Calls puts and prints string
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
