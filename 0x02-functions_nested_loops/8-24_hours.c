2c0554cce7bfbb9a49763102174aa7eb044030ba
Your Name <lewisjassy43@gmail.com>
2023-03-21 15:38:47 -0700

    today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,22 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * jack_bauer - print every minute of the day from 00:00 to 23:59
+ *jack_bauer -  count down 24hrs
  *
- * Return: void
+ *Return: void.
  */
-void jack_bauer(void)
+void  jack_bauer(void)
 {
-	int hr, min;
-
-	for (hr = 0; hr < 24; hr++)
-	{
-		for (min = 0; min < 60; min++)
-		{
-			_putchar(hr / 10 + '0');
-			_putchar(hr % 10 + '0');
-			_putchar(':');
-			_putchar(min / 10 + '0');
-			_putchar(min % 10 + '0');
-			_putchar('\n');
-		}
-	}
+int hour, minute;
+for (hour = 0; hour <= 23; hour++)
+{
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar('0' + (hour / 10));
+_putchar('0' + (hour % 10));
+_putchar(':');
+_putchar('0' + (minute / 10));
+_putchar('0' + (minute % 10));
+_putchar('\n');
+}
+}
 }
