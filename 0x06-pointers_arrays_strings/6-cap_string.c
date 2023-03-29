60a7d407394b13482102bfa4bc44e7d26334ed72
Your Name <lewisjassy43@gmail.com>
2023-03-29 15:19:48 -0700

    Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,39 @@
 #include "main.h"
 
 /**
- * string_toupper - convert lowercase letters to uppercase
- * @s: string to convert
- *
- * Return: pointer to `s'
+ * cap_string - capitalize all words of a string
+ * @str: string
+ * Return: `str`
  */
-char *string_toupper(char *s)
-{
-	int i = 0;
 
-	while (s[i])
+char *cap_string(char *str)
+{
+	int i, c;
+	int trigger;
+	char nots[] = ",;.!?(){}\n\t\" ";
+
+	for (i = 0, trigger = 0; str[i] != '\0'; i++)
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 'a' - 'A';
-		++i;
+		if (str[0] > 96 && str[0] < 123)
+			trigger = 1;
+		for (c = 0; nots[c] != '\0'; c++)
+		{
+			if (nots[c] == str[i])
+				trigger = 1;
+		}
+
+		if (trigger)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				trigger = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+				trigger = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				trigger = 0;
+		}
 	}
-	return (s);
+	return (str);
 }
