112ab259ae089b748ea1732572c361b11e034ed2
Your Name <lewisjassy43@gmail.com>
2023-03-29 15:51:33 -0700

    Numbers have life

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer;
- * @n: integer to be printed;
+ * print_number - print an integer as a string char by char
+ * @n: int to print
  */
-
 void print_number(int n)
 {
-	unsigned int n1;
+	int sign = (n < 0 ? -1 : 1);
 
-	if (n < 0)
+	if (!(n / 10))
 	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
+		if (sign < 0)
+			_putchar('-');
+		_putchar(n * sign + '0');
 	}
-
-	if (n1 / 10)
-		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
+	else
+	{
+		print_number(n / 10);
+		_putchar(n % 10 * sign + '0');
+	}
 }
