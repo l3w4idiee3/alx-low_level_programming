ffeaa33768ef3c62a42f27776c777ac5505acb51
Your Name <lewisjassy43@gmail.com>
2023-03-29 14:53:55 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reverse array `a' of length `n'
+ * @a: array to reverse
+ * @n: length of array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < --n; ++i)
+	{
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+	}
+}
