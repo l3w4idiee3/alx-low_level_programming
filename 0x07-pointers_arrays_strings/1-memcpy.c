c392223c0439339667469a8032058aed0a805c60
Your Name <lewisjassy43@gmail.com>
2023-04-03 12:44:40 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        int i = 0;
+	int i = 0;
 
-        while (n && (dest[i] = src[i]))
-                ++i, --n;
-        while (n-- > 0)
-                dest[i++] = '\0';
-        return (dest);
+	while (n && (dest[i] = src[i]))
+		++i, --n;
+	while (n-- > 0)
+		dest[i++] = '\0';
+	return (dest);
 }
