34dd13049cd9ce04320ca02efa1254597d80bbc3
Your Name <lewisjassy43@gmail.com>
2023-04-05 13:08:24 -0700

    Once an idea has taken hold of the brain

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,6 +4,8 @@
  * _pow_recursion - printing a function x to the power of y
  * @x: integer to be returned
  * @y: integer to be powered to
+ *
+ * Return: x * _pow_recursion(x, y - 1)
  */
 int _pow_recursion(int x, int y)
 {
