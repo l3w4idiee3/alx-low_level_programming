ac528a9ba5cdc4ebbb0cdccec92f2989fe9c396b
Your Name <lewisjassy43@gmail.com>
2023-04-09 14:14:38 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - multiply two numbers from command line
+ * @argc: number of arguments
+ * @argv: array of argumetns
+ *
+ * Return: 0 for success, 1 if error
+ */
+
+int main(int argc, char *argv[])
+	{
+	int sum = 0;
+	int i = 1;
+	int j = 0;
+
+	if (argc == 1)
+	{
+	printf("%d\n", sum);
+	return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		char *num_str = argv[i];
+
+		for (j = 0; num_str[j] != '\0'; j++)
+		{
+		if (!isdigit(num_str[j]))
+		{
+		printf("Error\n");
+		return (1);
+	}
+	}
+	sum = atoi(num_str);
+
+	if (sum < 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum += sum;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
