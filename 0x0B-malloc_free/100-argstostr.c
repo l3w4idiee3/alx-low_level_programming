d1f072f7cb1e8e42ec80cb5470e25e71270957e4
Your Name <lewisjassy43@gmail.com>
2023-04-11 17:16:44 -0700

    mountains ahead to climb that wear you out

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenate strings pointed to by `av'
+ * @ac: number of strings
+ * @av: array of strings
+ *
+ * Return: point to new string, or NULL if failure
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, totalbytes = 0;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; ++i)
+	{
+		for (j = 0; *(*(av + i) + j); ++j)
+			;
+		totalbytes += j;
+		++totalbytes; /* space for newline */
+	}
+	++totalbytes; /* space for string terminator */
+
+	p = (char *) malloc(totalbytes * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0, k = 0; i < ac; ++i, ++k)
+	{
+		for (j = 0; *(*(av + i) + j); ++j, ++k)
+			*(p + k) = *(*(av + i) + j);
+		*(p + k) = '\n';
+	}
+	*(p + k + 1) = '\0';
+	return (p);
+}
