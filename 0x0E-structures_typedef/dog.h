ee0e918dcacc4c56422b565cedce59b98ca118e7
Your Name <lewisjassy43@gmail.com>
2023-04-17 13:38:37 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef STRTYP
+#define STRTYP
 /**
  * struct dog - structure containing characteristics of a dog
  * @name: name of dog
@@ -16,4 +16,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* _DOG_H_ */
+#endif /*STRTYP */
