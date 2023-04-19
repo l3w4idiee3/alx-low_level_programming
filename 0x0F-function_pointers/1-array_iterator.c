abff0c63decdec2647452064a47daaf4cc58c24c
Your Name <lewisjassy43@gmail.com>
2023-04-19 09:57:25 -0700

    Do not think much about something just do it

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,18 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+
 /**
- * array_iterator - a function executing a function given as a par
- * @array: a pointer to the first element of an array of intergers
- * @size: the number of elementsin the array
- * @action: a pointer to a function
+ * array_iterator - execute `action' on every element of `array'
+ * @array: array to modify
+ * @size: size of `array'
+ * @action: function to call on elements of `array'
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-	{
-		(*action)(array[i]);
-	}
+	if (action == NULL || array == NULL || size == 0)
+		exit(EXIT_FAILURE);
+	for (i = 0; i < size; ++i)
+		action(array[i]);
 }
