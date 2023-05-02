4902f59f32f6c773b820867fd32b83d93a83c5dc
Your Name <lewisjassy43@gmail.com>
2023-05-02 15:42:17 -0700

    sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * sum_listint - function thatb  return sum of data n
+ * @head: pointer to the start of the list
+ * Return: 0
+ */
+int sum_listint(listint_t *head)
+{
+	int n = 0;
+
+	while (head != NULL)
+	{
+		n += head->n
+		head = head->next;
+	}
+	return (0);
+}
