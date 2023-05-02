7a1bc6d56443c60c4c9fcf944d6ea974c757b60d
Your Name <lewisjassy43@gmail.com>
2023-05-02 09:19:15 -0700

    free a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - freeing a list
+ * @head: pointer to the start of the line
+ * Return: free
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *rev;
+	{
+	while ((rev = head) != NULL)
+		head = head->next;
+	}
+	free(rev);
+}
