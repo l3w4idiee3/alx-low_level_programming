91a5504570a05ded411067618265fa8678403b1b
Your Name <lewisjassy43@gmail.com>
2023-05-02 13:39:39 -0700

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - function that frees a list
+ * @head: pointer to the start  of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *prev;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		prev = (*head)->next;
+		free(head);
+		*head = prev;
+	}
+}
