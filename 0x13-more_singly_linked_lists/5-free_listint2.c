8a16f65b3a7a7e410ecf92427108bc5080f9504e
Your Name <lewisjassy43@gmail.com>
2023-05-02 13:54:39 -0700

    free1

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *prev;
 
-	if (head == NULL)
-		return;
-	while (*head)
+	while (*head != NULL)
 	{
-		*head = (*head)->next;
 		prev = *head;
+		*head = (*head)->next;
 		free(prev);
 	}
+	*head = NULL;
 }
